c1ba38408e5451f5b73aa689805fde9efb426e78
Thierry Bernier <guillaumeportails@users.noreply.github.com>
2018-05-10 17:35:29 +0200

    Update define.c

--- define.c
+++ define.c
@@ -1,9 +1,9 @@
-// C preprocessor abuse trick
+// C preprocessor abuse trick (as old as the C pre-processor is)
 
 #include <stdio.h>
 
 
-#define LISTE(tuple) \
+#define MYLIST(tuple) \
   /*    var siz  type    dft   unit   */   \
   tuple(D,    1, float,  1.2,  "m")	\
   tuple(V,    1, float,  2.3,  "m/s")	\
@@ -15,18 +15,18 @@
 struct s
 {
 # define GEN_STRUCT(var,size,type,dft,unit)		type var[size];
-  LISTE(GEN_STRUCT)
+  MYLIST(GEN_STRUCT)
 };
 
 
 #define GEN_PARAM(var,size,type,dft,unit)		type var,
-void toto (struct s *s, LISTE(GEN_PARAM) int end)
+void foo (struct s *s, MYLIST(GEN_PARAM) int end)
 {
 # define GEN_AFFECT(var,size,type,dft,unit)		{ for(int i=0; i<size; i++) s->var[i] = var; }
-  LISTE(GEN_AFFECT);
+  MYLIST(GEN_AFFECT);
    
 # define GEN_PRINT(var,size,type,dft,unit)		printf(" " #var " = %f ... " unit "\n", (float) s->var[0]);
-  LISTE(GEN_PRINT);
+  MYLIST(GEN_PRINT);
 }
 
 
@@ -35,7 +35,7 @@ int main (int argc, char *argv[])
   struct s s;
   
 # define GEN_CALL(var,size,type,dft,unit)		dft,
-  toto (&s, LISTE(GEN_CALL) 1234);
+  foo (&s, MYLIST(GEN_CALL) 1234);
 
   return 0;
 }
